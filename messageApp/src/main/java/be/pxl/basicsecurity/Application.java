738f620f46024bc4479352d36e9e6408994c3fb8
stefmorren <stefmorren@gmail.com>
2017-03-23 16:53:16 +0100

    Init spring

---
+++ messageApp/src/main/java/be/pxl/basicsecurity/Application.java
@@ -0,0 +1,14 @@
+package be.pxl.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by Stef on 23/03/2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
