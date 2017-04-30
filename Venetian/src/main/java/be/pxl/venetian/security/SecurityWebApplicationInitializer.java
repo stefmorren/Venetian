4dcd52a7f7f2ff36914a7c3d4605c0f207f99139
stefmorren <stefmorren@gmail.com>
2017-04-30 20:38:28 +0200

    pushing .gitignore

--- Venetian/src/main/java/be/pxl/venetian/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package be.pxl.venetian.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
