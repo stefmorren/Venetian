dfd5c6d292a5aa127d029d79132ff018fe9753df
stefmorren <stefmorren@gmail.com>
2017-05-01 17:01:58 +0200

    added stego

---
+++ Venetian.BusinessLayer/SteganographyUtility.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venetian.BusinessLayer
+{
+    public class SteganographyUtility
+    {
+        private static byte[] _salt = Encoding.ASCII.GetBytes("jasdh7834y8hfeur73rsharks214");
+
+        public enum State
+        {
+            Hiding,
+            Filling_With_Zeros
+        };
+
+        public static Bitmap embedText(string text, Bitmap bmp)
+        {
+            // initially, we'll be hiding characters in the image
+            State state = State.Hiding;
+
+            // holds the index of the character that is being hidden
+            int charIndex = 0;
+
+            // holds the value of the character converted to integer
+            int charValue = 0;
+
+            // holds the index of the color element (R or G or B) that is currently being processed
+            long pixelElementIndex = 0;
+
+            // holds the number of trailing zeros that have been added when finishing the process
+            int zeros = 0;
+
+            // hold pixel elements
+            int R = 0, G = 0, B = 0;
+
+            // pass through the rows
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                // pass through each row
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    // holds the pixel that is currently being processed
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    // now, clear the least significant bit (LSB) from each pixel element
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+
+                    // for each pixel, pass through its elements (RGB)
+                    for (int n = 0; n < 3; n++)
+                    {
+                        // check if new 8 bits has been processed
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            // check if the whole process has finished
+                            // we can say that it's finished when 8 zeros are added
+                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            {
+                                // apply the last pixel on the image
+                                // even if only a part of its elements have been affected
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+
+                                // return the bitmap with the text hidden in
+                                return bmp;
+                            }
+
+                            // check if all characters has been hidden
+                            if (charIndex >= text.Length)
+                            {
+                                // start adding zeros to mark the end of the text
+                                state = State.Filling_With_Zeros;
+                            }
+                            else
+                            {
+                                // move to the next character and process again
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        // check which pixel element has the turn to hide a bit in its LSB
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        // the rightmost bit in the character will be (charValue % 2)
+                                        // to put this value instead of the LSB of the pixel element
+                                        // just add it to it
+                                        // recall that the LSB of the pixel element had been cleared
+                                        // before this operation
+                                        R += charValue % 2;
+
+                                        // removes the added rightmost bit of the character
+                                        // such that next time we can reach the next one
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+
+                        pixelElementIndex++;
+
+                        if (state == State.Filling_With_Zeros)
+                        {
+                            // increment the value of zeros until it is 8
+                            zeros++;
+                        }
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+        public static string extractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            // holds the text that will be extracted from the image
+            string extractedText = String.Empty;
+
+            // pass through the rows
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                // pass through each row
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    // for each pixel, pass through its elements (RGB)
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    // get the LSB from the pixel element (will be pixel.R % 2)
+                                    // then add one bit to the right of the current character
+                                    // this can be done by (charValue = charValue * 2)
+                                    // replace the added bit (which value is by default 0) with
+                                    // the LSB of the pixel element, simply by addition
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+
+                        colorUnitIndex++;
+
+                        // if 8 bits has been added, then add the current character to the result text
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            // reverse? of course, since each time the process happens on the right (for simplicity)
+                            charValue = reverseBits(charValue);
+
+                            // can only be 0 if it is the stop character (the 8 zeros)
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+
+                            // convert the character value from int to char
+                            char c = (char)charValue;
+
+                            // add the current character to the result text
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+
+        public static string EncryptStringAES(string plainText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                throw new ArgumentNullException("plainText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+            string outStr = null;                       // Encrypted string to return
+            RijndaelManaged aesAlg = null;              // RijndaelManaged object used to encrypt the data.
+
+            try
+            {
+                // generate the key from the shared secret and the salt
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
+
+                // Create a RijndaelManaged object
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    // prepend the IV
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return outStr;
+        }
+
+        /// <summary>
+        /// Decrypt the given string.  Assumes the string was encrypted using 
+        /// EncryptStringAES(), using an identical sharedSecret.
+        /// </summary>
+        /// <param name="cipherText">The text to decrypt.</param>
+        /// <param name="sharedSecret">A password used to generate a key for decryption.</param>
+        public static string DecryptStringAES(string cipherText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                throw new ArgumentNullException("cipherText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+            // Declare the RijndaelManaged object
+            // used to decrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            try
+            {
+                // generate the key from the shared secret and the salt
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
+
+                // Create the streams used for decryption.                
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                {
+                    // Create a RijndaelManaged object
+                    // with the specified key and IV.
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    // Get the initialization vector from the encrypted stream
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+                    // Create a decrytor to perform the stream transform.
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return plaintext;
+        }
+        private static byte[] ReadByteArray(Stream s)
+        {
+            byte[] rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+            {
+                throw new SystemException("Stream did not contain properly formatted byte array");
+            }
+
+            byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+            if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+            {
+                throw new SystemException("Did not read byte array properly");
+            }
+
+            return buffer;
+        }
+    }
+}
--- Venetian/MessagePanel.xaml.cs
+++ Venetian/MessagePanel.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 using Venetian.BusinessLayer;
 using Venetian.DataLayer;
 using Venetian.DomainClasses;
@@ -27,6 +30,8 @@ namespace Venetian
         private MessageRepository _messageRepository;
         private User _user;
         private User _receiver;
+        private Bitmap _image;
+
         public MessagePanel(User user, VenetianContext venetianContext)
         {
             InitializeComponent();
@@ -35,6 +40,7 @@ namespace Venetian
             _user = user;
             FillReceiverList();
             SetConversationLayout(false);
+            resetSteganoForm(false);
         }
 
         private void FillReceiverList()
@@ -45,7 +51,7 @@ namespace Venetian
 
         private void listBoxReceivers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            _receiver = (User) listBoxReceivers.SelectedValue;
+            _receiver = (User)listBoxReceivers.SelectedValue;
             SetConversationLayout(true);
             LoadConversations();
 
@@ -58,12 +64,15 @@ namespace Venetian
             textBlockMessages.Text = "";
 
             foreach (Message message in messages)
-            {   
-                string decryptedText = AESUtility.DecryptStringFromBytes_Aes(message.EncryptedText, RSAUtility.RSADecrypt(message.Receiver.PrivateKey, message.EncryptedAesKey), message.IV);
+            {
+                string decryptedText = AESUtility.DecryptStringFromBytes_Aes(message.EncryptedText,
+                    RSAUtility.RSADecrypt(message.Receiver.PrivateKey, message.EncryptedAesKey), message.IV);
 
-                if (RSAUtility.RSAVerifySignedHash(Encoding.ASCII.GetBytes(decryptedText), message.RSAEncryptedHashedMessage, message.Sender.PublicKey))
+                if (RSAUtility.RSAVerifySignedHash(Encoding.ASCII.GetBytes(decryptedText),
+                    message.RSAEncryptedHashedMessage, message.Sender.PublicKey))
                 {
-                    textBlockMessages.Text += message.Date + "\n" + message.Sender.Username + ": " + decryptedText + "\n\n";
+                    textBlockMessages.Text += message.Date + "\n" + message.Sender.Username + ": " + decryptedText +
+                                              "\n\n";
                 }
                 else
                 {
@@ -95,20 +104,148 @@ namespace Venetian
                 message.Receiver = _receiver;
                 message.Sender = _user;
 
-                using(Aes myAes = Aes.Create())
+                using (Aes myAes = Aes.Create())
                 {
                     byte[] encrypted = AESUtility.EncryptStringToBytes_Aes(textBoxMessage.Text, myAes.Key, myAes.IV);
                     message.EncryptedText = encrypted;
                     message.IV = myAes.IV;
-                    message.EncryptedAesKey = RSAUtility.RSAEncrypt(_receiver.PublicKey, myAes.Key); 
+                    message.EncryptedAesKey = RSAUtility.RSAEncrypt(_receiver.PublicKey, myAes.Key);
                 }
                 //Generate hash from original message & encrypted with RSA for signing
-                message.RSAEncryptedHashedMessage = RSAUtility.RSASign(_user.PrivateKey, Encoding.ASCII.GetBytes(textBoxMessage.Text));
+                message.RSAEncryptedHashedMessage = RSAUtility.RSASign(_user.PrivateKey,
+                    Encoding.ASCII.GetBytes(textBoxMessage.Text));
 
                 _messageRepository.InsertMessage(message);
                 textBoxMessage.Text = "";
                 LoadConversations();
-            }    
+            }
+        }
+
+        private void buttonOpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            resetSteganoForm(false);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string startdir = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.InitialDirectory = startdir;
+            openFileDialog.Filter = "Image files (*.png, *.jpg, *.bmp) | *.png; *.jpg; *.bmp";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imageBox.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                _image = new Bitmap(openFileDialog.FileName);
+                textBoxImageLocation.Text = openFileDialog.FileName;
+            }
+            resetSteganoForm(true);
+        }
+
+        private void buttonEncode_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxText.Text.Length == 0)
+            {
+                MessageBox.Show("The text you want to hide can't be empty", "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+            else
+            {
+                if (checkBoxEncrypted.IsChecked == true)
+                {
+                    if (passwordBox.Password.Length < 6)
+                    {
+                        MessageBox.Show("Password must have at least 6 characters.", "Error", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                    }
+                    else
+                    {
+                        _image =
+                            SteganographyUtility.embedText(
+                                SteganographyUtility.EncryptStringAES(textBoxText.Text, passwordBox.Password),
+                                _image);
+                        textBoxText.Text = "";
+                    }
+                }
+                else
+                {
+                    _image =
+                        SteganographyUtility.embedText(
+                            textBoxText.Text,
+                            _image);
+                    textBoxText.Text = "";
+                }
+            }
+
+        }
+
+        private void checkBoxEncrypted_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.IsEnabled = true;
+        }
+
+        private void checkBoxEncrypted_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.IsEnabled = false;
+        }
+
+        private void buttonDecode_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxEncrypted.IsChecked == true)
+            {
+                try
+                {
+                    textBoxText.Text = SteganographyUtility.DecryptStringAES(SteganographyUtility.extractText(_image), passwordBox.Password);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Wrong password!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                textBoxText.Text = SteganographyUtility.extractText(_image);
+            }
+        }
+
+        private void buttonSaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+
+            if (save_dialog.ShowDialog() == true)
+            {
+                switch (save_dialog.FilterIndex)
+                {
+                    case 1:
+                        {
+                            _image.Save(save_dialog.FileName, ImageFormat.Png);
+                        }
+                        break;
+                    case 2:
+                        {
+                            _image.Save(save_dialog.FileName, ImageFormat.Bmp);
+                        }
+                        break;
+                }
+            }
+            textBoxText.Text = "";
+            MessageBox.Show("Images succesfully saved.", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        private void resetSteganoForm(bool value)
+        {
+            if (value == false)
+            {
+                _image = null;
+                textBoxText.Text = "";
+                passwordBox.Password = "";
+                textBoxImageLocation.Text = "";
+                passwordBox.IsEnabled = false;
+                checkBoxEncrypted.IsChecked = false;
+            }         
+            buttonDecode.IsEnabled = value;
+            buttonEncode.IsEnabled = value;
+            buttonSaveImage.IsEnabled = value;
+            textBoxText.IsEnabled = value;
+            checkBoxEncrypted.IsEnabled = value;
+
         }
     }
 }
